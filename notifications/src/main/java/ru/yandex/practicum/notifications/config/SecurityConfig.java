6071623a19f67f37deb2563a2fb679d93035a702
Margarita <rl-c@yandex.ru>
2026-05-27 22:47:27 +0300

    Init notification service

---
+++ notifications/src/main/java/ru/yandex/practicum/notifications/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.notifications.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/notifications/**").hasAuthority("SCOPE_notifications.write")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
