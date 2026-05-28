8856da007067904908abb3645c1eba9eab44d6d1
Margarita <rl-c@yandex.ru>
2026-05-28 20:43:13 +0300

    Add tests for notifications service

--- notifications/src/main/java/ru/yandex/practicum/notifications/config/SecurityConfig.java
+++ notifications/src/main/java/ru/yandex/practicum/notifications/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +16,12 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/notifications/**").hasAuthority("SCOPE_notifications.write")
+                        .requestMatchers("/notifications").hasAuthority("SCOPE_notifications.write")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()))
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
